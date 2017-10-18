9e0b049a7b6390cecd5b708288a063bc04b48b89
bilel <bilel.kedidi@gmail.com>
2017-10-18 14:03:30 +0100

    digitco

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,38 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require jquery.js
+//= require jquery_ujs
+//= require jquery.easing.1.3.js
+//= require bootstrap.js
+//= require jquery.singlePageNav.min.js
+//= require wow.min.js
+//= require jquery.transit.js
+//= require jquery.jcountdown.js
+
+//= require owl.carousel.js
+//= require jquery.appear.js
+//= require jquery.magnific-popup.js
+//= require jquery.quicksand.js
+//= require contact.js
+//= require jquery.ticker.js
+//= require jquery.mb.YTPlayer.js
+//= require jquery.elevateZoom-3.0.8.min.js
+//= require lightbox.js
+
+
+//= require jquery.themepunch.plugins.min.js
+//= require jquery.themepunch.revolution.min.js
+//= require jquery.scrollTo.js
+
+//= require  main.js
