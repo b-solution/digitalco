dcdd06eb2bdd7a122855f13b189a149e9304ebd3
bilel <bilel.kedidi@gmail.com>
2017-11-02 14:05:31 +0100

    change sender

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def welcome_email(options={})
     @options = options
-    mail(to: 'bilel.kedidi@gmail.com', subject: "(digitalCo) Nouveau message de #{options[:email]} ")
+    mail(to: 'contact@digital-co.fr', subject: "(digitalCo) Nouveau message de #{options[:email]} ")
   end
 end
